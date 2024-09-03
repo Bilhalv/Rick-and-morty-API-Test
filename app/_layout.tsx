b677766abd17f7e67d4b5075c779ea6f24c709c9
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-09-03 18:57:53 +0000

    First Commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,54 @@
-import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function RootLayout() {
   return (
-    <View>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
+    <View style={styles.header}>
+      <FontAwesome style={styles.button} name="filter" size={20} color="white" />
+      <FontAwesome style={styles.button} name="sort" size={20} color="white" />
+      <View style={styles.search}>
+        <FontAwesome name="search" size={20} color="white" />
+        <TextInput
+          placeholder="Search"
+          editable
+          style={{
+            color: "#ffffff",
+            width: "100%",
+            height: "100%",
+            fontSize: 12
+          }}
+        />
+      </View>
+      <FontAwesome style={styles.button} name="plus" size={20} color="white" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    gap: 10
+  },
+  button: {
+    backgroundColor: "#3C3E44",
+    width: 40,
+    height: 40,
+    borderRadius: 100,
+    alignContent: "center",
+    textAlign: "center"
+  },
+  search: {
+    flex: 1,
+    height: 40,
+    gap: 10,
+    backgroundColor: "#3C3E44",
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10
+  }
+})
