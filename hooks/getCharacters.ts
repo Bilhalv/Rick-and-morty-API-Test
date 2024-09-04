cc3cf3f35b2d149bd9051b21b6ed072fe8e2eb8e
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-09-04 19:22:37 -0300

    feat: AsyncStorage

--- hooks/getCharacters.ts
+++ hooks/getCharacters.ts
@@ -1,9 +1,11 @@
 import { Person } from "@/types/Person";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default async function getCharacters(page: number): Promise<Person[]> {
     try {
         const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
         const json = await response.json();
+        await AsyncStorage.setItem('characters', JSON.stringify(json.results));
         return json.results as Person[];
     } catch (error) {
         console.error(error);
